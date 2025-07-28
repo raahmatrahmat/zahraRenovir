b568f33fe4e8252f30e7344e21cb0c378e6f9b65
raahmatrahmat <arahmatmuhammadnurulfala@gmail.com>
2025-07-28 19:02:04 +0700

    Fix: Rename main.dartimport to main.dart

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+void main() { runApp(const ZahraRenovirApp());
+}
+class ZahraRenovirApp extends StatelessWidget { 
+  const ZahraRenovirApp({super.key}); @override 
+  Widget build(BuildContext context) {
+    return MaterialApp( title: 'ZahraRenovir', 
+      home: Scaffold(
+        appBar: AppBar(title: const 
+        Text('ZahraRenovir')), body: const 
+        Center(child: Text('Hello, Zahra!')),
+      ), );
+  }
+}0
+
+x
+x0
+
